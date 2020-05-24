7973433e9f74e52d6ce723a72aa12d86ef43b5f3
tanmay <trshimpi25@gmail.com>
2020-05-25 00:11:25 +0530

    Updates

---
+++ precache-manifest.2db8d4037fe9cbc47641727d7c50d806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85986cee561350bd1a3cb12eefa2f180",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "d4ef6c7de03bc16d655a",
+    "url": "/monster-app/static/css/main.67bee0c1.chunk.css"
+  },
+  {
+    "revision": "f0031d39350e961c78cc",
+    "url": "/monster-app/static/js/2.6281f490.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.6281f490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ef6c7de03bc16d655a",
+    "url": "/monster-app/static/js/main.91280d34.chunk.js"
+  },
+  {
+    "revision": "9510c44ef8ba4578b3fe",
+    "url": "/monster-app/static/js/runtime-main.887ab741.js"
+  }
+]);
